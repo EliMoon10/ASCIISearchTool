719b21a314bebf3287dcd49341447b956b26752c
GijunMoon <gijunmoon@moonstudio.kro.kr>
2019-03-29 22:02:52 +0900

    코드

---
+++ SearchASCIITool/SearchASCIITool/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SearchASCIITool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Search = new System.Windows.Forms.Button();
+            this.SearchText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PrintCode = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Search
+            // 
+            this.Search.Location = new System.Drawing.Point(236, 56);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(75, 23);
+            this.Search.TabIndex = 0;
+            this.Search.Text = "찾기";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SearchText
+            // 
+            this.SearchText.Location = new System.Drawing.Point(13, 56);
+            this.SearchText.Name = "SearchText";
+            this.SearchText.Size = new System.Drawing.Size(217, 21);
+            this.SearchText.TabIndex = 1;
+            this.SearchText.TextChanged += new System.EventHandler(this.SearchText_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(66, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "해당 ASCII코드 입니다.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // PrintCode
+            // 
+            this.PrintCode.AutoSize = true;
+            this.PrintCode.Font = new System.Drawing.Font("굴림", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.PrintCode.Location = new System.Drawing.Point(51, 137);
+            this.PrintCode.Name = "PrintCode";
+            this.PrintCode.Size = new System.Drawing.Size(243, 20);
+            this.PrintCode.TabIndex = 3;
+            this.PrintCode.Text = "알맞은 문자를 검색하세요";
+            this.PrintCode.Click += new System.EventHandler(this.PrintCode_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 264);
+            this.Controls.Add(this.PrintCode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SearchText);
+            this.Controls.Add(this.Search);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ACSII코드 검색기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.TextBox SearchText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label PrintCode;
+    }
+}
+
